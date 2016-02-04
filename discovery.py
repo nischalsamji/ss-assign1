be82199965d4bb771ccd082ea961a9b3905a69b0
Nischal Samji <nischalsamji@gmail.com>
2016-02-03 22:43:46 -0700

    Adding files
    Added all files

--- discovery.py
+++ discovery.py
@@ -1,3 +1,7 @@
 import os
 
-print os.system('ls')
+
+ssh_files = ['/etc/hosts','~/.ssh/config','/etc/ssh/ssh_config','~/.ssh/authorized_keys','~/.ssh/known_hosts','/etc/ssh/ssh_known_hosts']
+for each_file in ssh_files:
+  file_content = read(each_file,"r")
+  print file_content.readlines()
